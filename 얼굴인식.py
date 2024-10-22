a4061f6960ac58d986cb68688016e92a607bfd2c
JWP-bangwahu <garook@e-mirim.hs.kr>
2024-10-22 17:16:24 +0900

    일단 얼굴인식 완성

--- 얼굴인식.py
+++ 얼굴인식.py
@@ -29,3 +29,52 @@ for name in os.listdir(KNOWN_FACES_DIR):
         known_faces.append(encoding)
         known_names.append(name)
 
+print('학습 끝. 얼굴인식 시작')
+
+#테스트 이미지 로드
+test_image = face_recognition.load_image_file(IMAGE_TO_TEST)
+
+# 테스트 이미지에서 얼굴추출
+locations = face_recognition.face_locations(test_image, model=MODEL)
+encodings = face_recognition.face_encodings(test_image, locations)
+
+test_image = cv2.cvtColor(test_image, cv2.COLOR_BGRA2BGR)
+
+for face_encoding, face_location in zip(encodings, locations):
+    results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
+    match = None
+    if True in results:
+        match = known_names[results.index(True)]
+        print(f'-{match} from {results}')
+
+        top_left = (face_location[3], face_location[0])
+        bottom_right = (face_location[1], face_location[2])
+        color = name_to_color(match)
+        cv2.rectangle(test_image, top_left, bottom_right, color, FRAME_THICKNESS)
+        top_left = (face_location[3], face_location[2])
+        bottom_right = (face_location[1], face_location[2]+22)
+        cv2.rectangle(test_image, top_left, bottom_right, color, cv2.FILLED)
+        cv2.putText(test_image, match,
+                    (face_location[3] + 10, face_location[2] + 15),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    (200, 200, 200), FONT_THICKNESS)
+
+cv2.imshow('face recognition', test_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
